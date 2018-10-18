9e7f5d3a15eaa1d7d6bcacee4f815b841504d910
LozovayaOlga <LoozvayaOlga@yandex.ru>
2018-10-18 20:43:27 +1000

    после проверки изенения

--- lesson 2/js/main.js
+++ lesson 2/js/main.js
@@ -1,89 +1,74 @@
 // **********************ЗАДАНИЕ 1	***********8
 
 var a = 1, b = 1, c, d;
-c = ++a; alert("c = " + c); // 2 , префиксый инкремент a=a+1=1+1=2 c=a=2 // теперь a=2
-d = b++; alert("d = " + d); // 1, Постфиксный инкремент- вычисляетья после инструкции b=1 d=b=1, ПОСЛЕ b=b+1=1+1=2
-c = (2+ ++a); alert("c = " + c); // 5, c=2+(a+1)=2+(2+1)=5 // теперь a=3
-d = (2+ b++); alert("d = " + d); // 4, d=2+b=2+2=4 ПОСЛЕ b=b+1=2+1=3
-alert("a = " + a); // 3
-alert("b = " + b); // 3
+c = ++a; console.log("c = " + c); // 2 , префиксый инкремент a=a+1=1+1=2 c=a=2 // теперь a=2
+d = b++; console.log("d = " + d); // 1, Постфиксный инкремент- вычисляетья после инструкции b=1 d=b=1, ПОСЛЕ b=b+1=1+1=2
+c = (2+ ++a); console.log("c = " + c); // 5, c=2+(a+1)=2+(2+1)=5 // теперь a=3
+d = (2+ b++); console.log("d = " + d); // 4, d=2+b=2+2=4 ПОСЛЕ b=b+1=2+1=3
+console.log("a = " + a); // 3
+console.log("b = " + b); // 3
 
 // **********************ЗАДАНИЕ 2	
 var a = 2;
 var x = 1 + (a *= 2); // x=1+(a*2)=1+(2*2)=5
-alert("x = " + x); // 5
+console.log("x = " + x); // 5
 
 // **********************ЗАДАНИЕ 3
 
-var a = prompt("введите a"); 
-var b = prompt("введите b");
+var a = 15; 
+var b = 15;
 var result;
 if (a >= 0 && b >= 0 ){
 			result = a - b ; 
-			alert("a-b= " + result);
+			console.log("a-b= " + result);
 		} else if (a < 0 && b < 0) {
 			result = a * b ;
-			alert("a*b= " + result);
+			console.log("a*b= " + result);
 		} else if ( (a >= 0 && b < 0) || (b >= 0 && a < 0)) {
 			result = parseInt(a) + parseInt(b) ;
-			alert("a+b= " + result);			
+			console.log("a+b= " + result);			
 		} else
-					alert("вы ввели не числа");
+					console.log("вы ввели не числа");
 
 
 
 // **********************ЗАДАНИЕ 4
 
-var a = prompt("введите a от 1 до 15");
+var a = 8;
 
 switch (a){
 	case '1' : 
-		alert('1');
-		break;
+		console.log('1');	
 	case '2': 
-		alert('1,2');
-		break;
+		console.log('2');	
 	case '3': 
-		alert('1,2,3');
-		break;
+		console.log('3');
 	case '4': 
-		alert('1,2,3,4');
-		break;
+		console.log('4');
 	case '5': 
-		alert('1,2,3,4,5');
-		break;
+		console.log('5');
 	case '6': 
-		alert('1,2,3,4,5,6');
-		break;
+		console.log('6');	
 	case '7': 
-		alert('1,2,3,4,5,6,7');
-		break;	
+		console.log('7');
 	case '8': 
-		alert('1,2,3,4,5,6,7,8');
-		break;
+		console.log('8');
 	case '9': 
-		alert('1,2,3,4,5,6,7,8,9');
-		break;	
+		console.log('9');
 	case '10': 
-		alert('1,2,3,4,5,6,7,8,9,10');
-		break;	
+		console.log('10');	
 	case '11': 
-		alert('1,2,3,4,5,6,7,8,9,10,11');
-		break;	
+		console.log('11');
 	case '12': 
-		alert('1,2,3,4,5,6,7,8,9,10,11,12');
-		break;	
+		console.log('12');
 	case '13': 
-		alert('1,2,3,4,5,6,7,8,9,10,11,12,13');
-		break;	
+		console.log('13');
 	case '14': 
-		alert('1,2,3,4,5,6,7,8,9,10,11,12,13,14');
-		break;	
+		console.log('14');
 	case '15': 
-		alert('1,2,3,4,5,6,7,8,9,10,11,12,13,14,15');
-		break;		
+		console.log('15');
 default : 
-		alert("число не от 1 до 15");
+		console.log("число не от 1 до 15");
 }
 // **********************ЗАДАНИЕ 5		
 var a ;
@@ -110,9 +95,9 @@ function division (a,b) {
 
 // **********************ЗАДАНИЕ 6
 
-var arg1= prompt("число");
-var arg2= prompt("число");
-var operation= prompt("операция  division multiplication minus  plus");
+var arg1 = -7;
+var arg2 = 8;
+var operation = division;//("операция  division multiplication minus  plus");
 var result;
  function mathOperation(arg1, arg2, operation){
 	switch(operation) {
@@ -128,12 +113,12 @@ var result;
 		case 'multiplication':
 		result = multiplication (arg1, arg2);
 		break;
-		default: alert('данные введены некорректно');
+		default: console.log('данные введены некорректно');
 	}
  return result;
  }
 
-alert(mathOperation(arg1, arg2, operation));
+console.log(mathOperation(arg1, arg2, operation));
 
 // **********************ЗАДАНИЕ 7
 
@@ -144,15 +129,21 @@ null == О false Значения null не преобразуются для с
 */
 // **********************ЗАДАНИЕ 8	
 
-var val= prompt("число");
-var pow= prompt("число");
+var val = 2;
+var pow = -3;
 
 function power(val, pow) {
+	if(pow == 0) {
+		return 1;
+	}
+	if(pow < 0) {
+		return power(1 / val, -pow);
+	}
 	if (pow != 1){
-		return val * power(val, pow-1);
+		return val * power(val, --pow);
 	} else
 		return val;
 	
 }
 	
-alert(power(val, pow));	
+console.log(power(val, pow));	
