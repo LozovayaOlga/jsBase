96d0abf34caba09c6e7b2b123dc8a36ec0a6d68e
LozovayaOlga <LoozvayaOlga@yandex.ru>
2018-10-25 23:14:36 +1000

    шахматная доска рисуется таблицей

--- lesson 5/script.js
+++ lesson 5/script.js
@@ -1,19 +1,16 @@
-var maindiv = document.createElement('div');
-maindiv.className = "maindiv";
-document.body.appendChild(maindiv);
-
-for (var i = 0; i < 64; i++) {
-	var mainDiv = document.querySelector('.maindiv');
-	var div = document.createElement('div');
-	if (i % 2) {
-		div.className = "white";
-
-	} else {
-			div.className = "black";
-
-	}
-	mainDiv.appendChild(div);
+var table = document.createElement("table");
+for (var i = 1; i < 9; i++) {
+    var tr = document.createElement('tr');
+    for (var j = 1; j < 9; j++) {
+        var td = document.createElement('td');
+        if (i%2 == j%2) {
+            td.className = "white";
+        } else {
+            td.className = "black";
+        }
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
 }
-
-
+document.body.appendChild(table);
 
